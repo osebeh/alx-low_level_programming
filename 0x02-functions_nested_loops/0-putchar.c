b2dc2a12b23ff53553392146e017515d9f77a499
Osebeh <osebehk@gmail.com>
2023-02-21 07:21:47 -0800

    first

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * Program prints _putchar and returns 0
+ * main() - primary function
+*/
+
+int main(void)
+{
+/*printing directly without declaring a variable */
+
+printf("_putchar");
+
+return (0);
+}
