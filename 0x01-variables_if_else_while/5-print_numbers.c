018c1b6465dc83c9fc98026b80e35061825c9d60
Hiram <osebehk@gmail.com>
2023-12-15 19:02:09 -0800

    Third

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Return: 0
+ */
 int main(void)
 {
 	int num = 0;
 
 	while (num < 10)
 	{
-		putchar(num + 'O');
+		putchar(num + '0');
 		num++;
 	}
 	putchar('\n');
-
 	return (0);
 }
