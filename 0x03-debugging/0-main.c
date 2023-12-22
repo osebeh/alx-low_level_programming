d29531261aa7951097672be0b77afab028520d72
Hiram <osebehk@gmail.com>
2023-12-22 07:26:14 -0800

    Correction on spacing around i

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * main - entry point & prints if input is -/+
  * Return: 0
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	i=0;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
