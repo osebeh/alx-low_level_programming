a4a5678d8819c64438522db628eccf144735a6f5
Hiram <osebehk@gmail.com>
2024-01-18 17:35:05 -0800

    First

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @s: input int
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *spc;
+
+	spc = malloc(b);
+
+	if (spc ==NULL)
+		exit(98);
+
+	return (spc);
+}
