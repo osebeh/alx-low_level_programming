7f755771098a2e6f48b05e46e3bc44acb71c782b
Hiram <osebehk@gmail.com>
2024-01-18 17:55:49 -0800

    First

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _calloc - allocates memory for an array of nmemb elements of 
+ * size bytes each and returns a pointer to the allocated memory
+ * @nmemb: memory spaces to eb allocated
+ * @size: size of the memory spaces
+ * Return: NULL on fail or pointer to allocated space
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *spc;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	spc = malloc(nmemb * size);
+
+	if (spc == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		spc[i] = 0;
+
+	return (spc);
+}
