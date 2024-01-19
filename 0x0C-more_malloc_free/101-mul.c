3427734a506091ee939bcedb06e7dd2a24a5c6dc
Hiram <osebehk@gmail.com>
2024-01-18 18:37:42 -0800

    First

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function that multiplies two numbers
+ * @i: arguments
+ * @j: arguments
+ * Return: 0
+ */
+int main(int i, int **j)
+{
+	int num1, num2, total;
+
+	if (i != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(j[1]);
+		num2 = atoi(j[2]);
+		total = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
+}
